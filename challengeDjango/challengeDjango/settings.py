a4bc24ad71d44f815238f29d5c3f2eca4ab2e2a8
Giuli Latin <giuly_latin@hotmail.com>
2024-04-21 14:24:24 -0300

    configure allow hostsç

--- challengeDjango/challengeDjango/settings.py
+++ challengeDjango/challengeDjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xbw_n84uhevox@8efi$zyvlnmgm91!^-fx3x&npy@f)^he$e+9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
